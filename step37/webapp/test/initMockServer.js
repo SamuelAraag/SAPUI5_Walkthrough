1ebcb0784123137d99a57e6542255a2c59477b18
unknown <samoelgaaradossantos@gmail.com>
2022-07-04 14:13:48 -0300

    TraduzindoSite

--- step37/webapp/test/initMockServer.js
+++ step37/webapp/test/initMockServer.js
@@ -3,9 +3,7 @@ sap.ui.define([
 ], function (mockserver) {
 	"use strict";
 
-	// initialize the mock server
 	mockserver.init();
 
-	// initialize the embedded component on the HTML page
 	sap.ui.require(["sap/ui/core/ComponentSupport"]);
 });
